01e798bd1ad0877eca5fcb51a4b9a496db0985e1
Kingcode <136673119+o2real@users.noreply.github.com>
2024-07-22 20:59:23 +0900

    file route

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# with open("my_text.txt") as file:
-#     contents = file.read()
-#     print(contents)
-#
-# with open("new_file.txt", mode="w") as file:
-#     file.write("New text.")
+with open("/Users/o2rea/Desktop/my_text.txt") as file:
+    contents = file.read()
+    print(contents)
